dc6e114b7da59e90d4edc6b6368cf380eae73d5c
Eddy Almeida <eddyalmeidapro@gmail.com>
2018-05-16 11:34:04 -0700

    Scanner in javascript

--- test/discovery.js
+++ test/discovery.js
@@ -2,6 +2,13 @@ var Discovery = require('../index.js').Discovery;
 
 var discover = new Discovery();
 
+/* Promise example
 discover.scan().then((data) => {
+	console.log(data);
+});
+*/
+
+discover.scan((err, data) => {
+	console.log(err);
 	console.log(data);
 });
