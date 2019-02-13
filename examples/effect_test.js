e455a323deca17a24d6281ebe48f195fc3ef111f
jangxx <janscheiper95@gmail.com>
2019-02-14 00:54:18 +0100

    rewrote the effect interface to use promises, changed the example to better reflect an actual use-case

--- examples/effect_test.js
+++ examples/effect_test.js
@@ -1,63 +1,41 @@
-var MHControl = require('../').Control;
+const { Control } = require('../');
 
-const characteristics = Object.freeze({
-	type1: {
-		rgb_min_0: true,
-		ww_min_0: true,
-		set_color_magic_bytes: [0xf0, 0x0f],
-		wait_for_reply: true
-	},
-	type2: {
-		rgb_min_0: false,
-		ww_min_0: false,
-		set_color_magic_bytes: [0x00, 0x0f],
-		wait_for_reply: false
-	},
-});
-
-var ip = process.argv[2];
-var type = (process.argv.length >= 4) ? process.argv[3] : "type1";
-var chars = (characteristics[type] != undefined) ? characteristics[type] : characteristics["type1"];
+const ip = process.argv[2];
+const wait_for_reply = (process.argv.length > 2) ? (process.argv[3] == "true") : false;
 
 if(ip == undefined) {
-	console.log("Usage: node effect_test.js <ip> [type]");
+	console.log("Usage: node effect_test.js <ip> [wait_for_reply]");
+	console.log();
+	console.log("Demostrates the use of the effect interface, by showing a simple interval function which changes the color roughly once a second");
 	process.exit();
 }
 
-var c = new MHControl(ip, chars);
+const control = new Control(ip, { wait_for_reply });
 
-var on = false;
-var need_delay = false;
+var last_change = 0;
+var last_color = 0;
 
-c.startEffectMode(function(effects) {
-	effects.start(function() {
-		var seconds = (new Date()).getSeconds();
-
-		if(need_delay) {
-			effects.delay(1000);
-			need_delay = false;
-			return;
-		}
-
-		if(!on) {
-			if(is_between(seconds, 0, 19)) {
-				effects.setColor(255, 0, 0);
-			} else if(is_between(seconds, 20, 39)) {
-				effects.setColor(0, 255, 0);
-			} else if(is_between(seconds, 40, 59)) {
-				effects.setColor(0, 0, 255);
-			}
-
-			on = true;
-			need_delay = true;
-		} else {
-			effects.setColor(0,0,0);
-			on = false;
-			need_delay = true;
-		}
-	});
+control.startEffectMode().then(effects => {
+	effects.start(interval_function);
 });
 
-function is_between(value, min, max) {
-	return (value >= min) && (value <= max);
+function interval_function() {
+	// this refers to the effect interface
+	let now = (new Date()).getTime();
+
+	if (now - last_change < 1000) {
+		return this.delay(Math.max(0, 1000 - (now - last_change)));
+	} else {
+		last_color = (last_color + 1) % 3;
+		last_change = now;
+
+		switch(last_color) {
+			case 0:
+				return this.setColor(255, 0, 0);
+			case 1:
+				return this.setColor(0, 255, 0);
+			case 2:
+				return this.setColor(0, 0, 255);
+		}		
+	}
 }
